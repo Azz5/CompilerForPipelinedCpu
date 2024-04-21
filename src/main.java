f18130d63db475fcf66f0b07a80972ca3dfb245c
ABDULAZIZ	ALSADRANI <s202159170@ccm.kfupm.edu.sa>
2024-04-21 17:30:12 +0300

    I-Type Done

--- src/main.java
+++ src/main.java
@@ -1,3 +1,4 @@
+
 import java.util.*;
 
 public class main {
@@ -10,52 +11,85 @@ public class main {
         //Scanner FileReader;
         //if (args.length != 0){
             String FileReader = "or 3 1 2";//FileReader = new Scanner(args[0]);
+            String FileReader1 = "and 4 2 5";
             //while (FileReader.hasNextLine()){
             System.out.println(parse(FileReader));
+        System.out.println(parse(FileReader1));
                 Instructions += parse(FileReader); //parse(FileReader.nextLine());
             //}
         //} else throw new RuntimeException("You must Enter the file name");
     }
     static String parse(String input){
+        String instruction = "";
         String[] instructionSpliced = input.split(" ");
         String output = "";
         if (instructionSpliced[0].matches(rTypeRegexZero)){
-            output += "0";
-            int data = Integer.parseInt(instructionSpliced[1]) + Integer.parseInt(instructionSpliced[2]) + Integer.parseInt(instructionSpliced[3]);
+            output += "0000";
+
+            if (Integer.toBinaryString(Integer.parseInt(instructionSpliced[1])).matches("^([0-1]{3})$")){
+                output += Integer.toBinaryString(Integer.parseInt(instructionSpliced[1]));
+            } else if (Integer.toBinaryString(Integer.parseInt(instructionSpliced[1])).matches("^([0-f]{2})$")) {
+                output += "0";
+                output += Integer.toBinaryString(Integer.parseInt(instructionSpliced[1]));
+            } else if (Integer.toBinaryString(Integer.parseInt(instructionSpliced[1])).matches("^([0-f]{1})$")) {
+                output += "00";
+                output += Integer.toBinaryString(Integer.parseInt(instructionSpliced[1]));
+            }
+
+            if (Integer.toBinaryString(Integer.parseInt(instructionSpliced[2])).matches("^([0-1]{3})$")){
+                output += Integer.toBinaryString(Integer.parseInt(instructionSpliced[2]));
+            } else if (Integer.toBinaryString(Integer.parseInt(instructionSpliced[2])).matches("^([0-f]{2})$")) {
+                output += "0";
+                output += Integer.toBinaryString(Integer.parseInt(instructionSpliced[2]));
+            } else if (Integer.toBinaryString(Integer.parseInt(instructionSpliced[2])).matches("^([0-f]{1})$")) {
+                output += "00";
+                output += Integer.toBinaryString(Integer.parseInt(instructionSpliced[2]));
+            }
+
+            if (Integer.toBinaryString(Integer.parseInt(instructionSpliced[3])).matches("^([0-1]{3})$")){
+                output += Integer.toBinaryString(Integer.parseInt(instructionSpliced[3]));
+            } else if (Integer.toBinaryString(Integer.parseInt(instructionSpliced[3])).matches("^([0-f]{2})$")) {
+                output += "0";
+                output += Integer.toBinaryString(Integer.parseInt(instructionSpliced[3]));
+            } else if (Integer.toBinaryString(Integer.parseInt(instructionSpliced[3])).matches("^([0-f]{1})$")) {
+                output += "00";
+                output += Integer.toBinaryString(Integer.parseInt(instructionSpliced[3]));
+            }
+
             switch (instructionSpliced[0]){
                 case "or":
+                    output += "000";
                     break;
                 case "and":
-                    data += 1;
+                    output += "001";
                     break;
                 case "nand":
-                    data += 2;
+                    output += "010";
                     break;
                 case "xor":
-                    data += 3;
+                    output += "011";
                     break;
                 case "add":
-                    data += 4;
+                    output += "100";
                     break;
                 case "sub":
-                    data += 5;
+                    output += "101";
                     break;
                 case "sge":
-                    data += 6;
+                    output += "110";
                     break;
                 case "sgeu":
-                    data += 7;
+                    output += "111";
                     break;
             }
-            String datatostring = Integer.toHexString(data);
-            if (datatostring.matches("^([0-f]{3})$")){
-                output+=datatostring;
-            } else if (datatostring.matches("^([0-f]{2})$")) {
-                output += "0";
-                output+=datatostring;
-            } else if (datatostring.matches("^([0-f]{1})$")) {
-                output += "00";
-                output+=datatostring;
+
+            instruction = Integer.toHexString(Integer.parseInt(output,2));
+            if (instruction.matches("^([0-f]{3})$")){
+                instruction = "0" + instruction;
+            } else if (instruction.matches("^([0-f]{2})$")) {
+                instruction = "0" + instruction;
+            } else if (instruction.matches("^([0-f]{1})$")) {
+                instruction = "0" + instruction;
             }
         } else if (instructionSpliced[0].matches(rTypeRegexOne)) {
             
@@ -66,7 +100,7 @@ public class main {
         }
 
 
-        return output;
+        return instruction;
     }
 
 }
